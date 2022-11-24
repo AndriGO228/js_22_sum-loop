47876c40bbad3daec5252444bb79e799cbf25b8c
andriGo <afomin129@gmail.com>
2022-11-25 00:19:29 +0200

    master

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sum(n) {
-	// write your code here
+	let num = 0;
+	let i = 1;
+	while (i <= n) {
+		num += i++;
+	}
+	return num;
+
+	// for (let i = 0; i <= n; i++) {
+	// 	num += i;
+	// }
+	// return num;
 }
 
 module.exports = sum;
